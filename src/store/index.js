e69eb757745118ea1beddfc9c5ee883ed2b281d5
Mehdidissem <mehdissem@gmail.com>
2023-03-08 19:35:43 +0100

    made a bit of data

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+const myProjects= [
+    {
+        title:"Delivair",
+        technologiesUsed:"React Native, Expo, NodeJS, ExpressJS, SocketIO",
+        description:"A fully functionnal mobile app",
+        picture:"https://res.cloudinary.com/dlnvacfsc/image/upload/v1678300240/Delivair_dxfymu.png"
+    },
+    {
+        title:"Delivair",
+        technologiesUsed:"React Native, Expo, NodeJS, ExpressJS, SocketIO",
+        description:"A fully functionnal mobile app",
+        picture:"https://res.cloudinary.com/dlnvacfsc/image/upload/v1678300240/Delivair_dxfymu.png"
+    }
+]
