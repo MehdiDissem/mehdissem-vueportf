ddd6c07dc87fc9c382cae8ed71165b15f4928d4d
Mehdidissem <mehdissem@gmail.com>
2023-03-08 21:41:43 +0100

    changed component to be mobile optimized

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,54 +1,79 @@
 <template>
-    <div id="about"> 
-      <v-row>
-          <v-col class="col-xs-12">            
-            <h2> ABOUT ME</h2>
+    <div id="about">
+      <v-container>
+        <v-row>
+            <v-col cols="12" sm="6" class="d-flex justify-center">
+  <v-img src="../assets/mehdidissem.png" max-width="400px" contain class="image-effect"></v-img>
+</v-col>
+          <v-col cols="12" sm="6">
+            <h2>ABOUT ME</h2>
             <p>
-            <p> 
-                As a Full Stack web developer, I possess a strong commitment to excellence, a collaborative mindset, and effective communication skills. 
-            </p> 
-            <p>
-                Drawing upon six years of experience across diverse marketing fields, including article writing, SEO, freelancing, and market analysis, I have honed a versatile set of technical and non-technical skills. 
-                </p>
-            <p>
-                I am now seeking new opportunities in the IT field to further enhance my technical abilities and contribute to innovative projects.
-
+              As a Full Stack web developer, I possess a strong commitment to excellence, a collaborative mindset, and effective communication skills.
             </p>
-        </p>
+            <p>
+              Drawing upon six years of experience across diverse marketing fields, including article writing, SEO, freelancing, and market analysis, I have honed a versatile set of technical and non-technical skills.
+            </p>
+            <p>
+              I am now seeking new opportunities in the IT field to further enhance my technical abilities and contribute to innovative projects.
+            </p>
           </v-col>
-          <v-col class="col-xs-12">
-              <v-img src="../assets/mehdidissem.png" max-width="500px" contain >
-
-              </v-img>
-          </v-col>
-      </v-row>
+        </v-row>
+      </v-container>
     </div>
-</template>
-
-<script>
-export default {
-    name : 'about'
-}
-</script>
-
-
-<style lang="scss" scoped > 
-    #about{
-        padding-left: 10%;
-
-       h2{  
-           text-transform: uppercase;
-           font-weight: bold;
-           color:var(--accent-color);
-           padding-top:15px;
-        }
-        p {
-             padding-block: 10px;
-             padding-right: 10px;
-             font-weight: 400;
-             color: var(--gray);
-             font-size: 1.3em;
-        }
+  </template>
+  
+  <script>
+  export default {
+    name: 'about',
+  };
+  </script>
+  
+  <style lang="scss" scoped>
+  #about {
+    padding: 15px;
+    h2 {
+      text-transform: uppercase;
+      font-weight: bold;
+      color: var(--accent-color);
+      padding-top: 15px;
+      font-size: 1.5rem;
     }
+    p {
+      padding-block: 10px;
+      padding-right: 10px;
+      font-weight: 400;
+      color: var(--gray);
+      font-size: 1.5rem;
+    }
+  }
+  .image-effect {
+    position: relative;
+    overflow: hidden;
+  }
 
-</style>
+  .image-effect img {
+    transition: all 0.3s ease-out;
+  }
+
+  .image-effect:hover img {
+    transform: scale(1.2);
+  }
+
+  .image-effect::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+    background: rgba(0, 0, 0, 0.5);
+    transition: all 0.3s ease-out;
+    pointer-events: none;
+  }
+
+  .image-effect:hover::before {
+    opacity: 1;
+  }
+  </style>
+  
