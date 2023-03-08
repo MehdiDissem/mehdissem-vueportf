e2cac01bd0f89e322fa615790b4b088a43c2872b
Mehdidissem <mehdissem@gmail.com>
2023-03-08 20:56:17 +0100

    looped over data and rendered cards, added styling

--- src/components/portfolio.vue
+++ src/components/portfolio.vue
@@ -1,61 +1,64 @@
 <template>
-    <v-container fluid class='ma-0 portfolio' id='portfolio'>
-        <h2 class=''>
-            Portfolio
-        </h2>
-        <v-container fluid class="projects pa-4">
-            <v-row align="center"
-                justify="center" >
-                <v-column 
-                    columns='12'
-                    sm='6'>
-                    <v-hover v-slot='{hover}'>
-                       <v-card                          
-                          class='mb-4 ml-1 grey project'  elevation="2"   round  >
-
-                          <v-img src=""  width='500px'  height='340px' >
-                               <!--' -->
-                              <v-expand-transition>
-                                <div
-                                    v-if="hover" style="height: 100%;"
-                                    class="d-flex flex-column transition-fast-in-fast-out  red darken-2 v-card--reveal  white--text pa-3 justify-center align-center card-reveal" >
-                                    <span class='my-2  mono' id='project_name'  > 
-                                        project name
-                                    </span>
-                                    <span class=' montserrat description '>
-                                        project description
-                                    </span>
-                                    <v-btn flat 
-                                    href="" target='_blank'
-                                    tile width='50%' class='mx-auto my-1 mt-2 '> 
-                                        Visit
-                                    </v-btn>
-                                    <div class='tech-icons d-flex flex-row flex-wrap' width='100%'> 
-                                        <v-img :src="icon_path(tech)"
-                                            width='30px'  contain    class='mx-2' id='tech-icon' >                                           
-                                        </v-img>
-                                    </div>
-                                </div>
-                              </v-expand-transition>                                   
-                          </v-img>
-                        </v-card>
-                     </v-hover>
-                 </v-column>
-            </v-row>
-         </v-container>
+    <v-container fluid class="ma-0 portfolio" id="portfolio">
+      <h2 class="">Portfolio</h2>
+      <v-container fluid class="projects pa-0 pa-md-4">
+        <v-row align="center" justify="center" class="mx-0">
+          <v-col cols="12" md="5" v-for="(project, index) in myProjects" :key="index">
+            <v-hover v-slot="{ hover }">
+              <v-card class="mb-4 grey project" elevation="2" round>
+                <v-img
+                  :src="project.picture"
+                  width="100%"
+                  max-height="340px"
+                  class="d-flex align-center justify-center"
+                >
+                  <v-expand-transition>
+                    <div
+                      v-if="hover"
+                      class="d-flex flex-column transition-fast-in-fast-out red darken-2 v-card--reveal white--text pa-3 justify-center align-center card-reveal"
+                    >
+                      <span class="my-2 mono" id="project_name">{{ project.title }}</span>
+                      <span class="montserrat description">{{ project.description }}</span>
+                      <v-btn flat :href="project.link" target="_blank" tile width="50%" class="mx-auto my-1 mt-2">Visit</v-btn>
+                      <div class="tech-icons d-flex flex-row flex-wrap" width="100%">
+                        <v-img
+                          :src="icon_path(tech)"
+                          v-for="(tech, techIndex) in project.technologiesUsed.split(',')"
+                          :key="techIndex"
+                          width="30px"
+                          height="30px"
+                          contain
+                          class="mx-2"
+                          id="tech-icon"
+                        ></v-img>
+                      </div>
+                    </div>
+                  </v-expand-transition>
+                </v-img>
+                <div class="pa-3">
+                  <span class="montserrat">{{ project.description }}</span>
+                </div>
+              </v-card>
+            </v-hover>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-container>
-</template>
+  </template>
 
 <script>
+import myProjects from "../store/index.js"
     export default { 
     name: 'portfolio ',components: {
 
     } ,
     mounted(){
         // this.$store.dispatch('getProjects')
+        console.log(this.myProject)
     },
     data () {
       return {
+        myProjects
             
       }
     },
@@ -76,63 +79,97 @@
 </script>
 
 <style scoped>
-
-.projects{
-    
-    margin:auto;
-    padding: 5%;
-    text-align: center;
-    overflow: hidden;
-    /* width: 33em;
-    min-width: 100vw;   */
-    /* height: 30vh; */
-    /* max-height: 60vh; */
-    /* max-width:95vw; */
+.projects {
+  margin: auto;
+  padding: 5%;
+  text-align: center;
+  overflow: hidden;
 }
 
-.project{
-  width: 30vw;
-  min-width:25rem;
-  height:auto;
-  margin:2%;
-  margin-left:12px;
-  margin-right:12px;
-
+.project {
+  width: 50%;
+  height: auto;
+  margin: 2%;
+  margin-left: auto;
+  margin-right: auto;
+  position: relative;
 }
 
-
-.portfolio>h2{
-
-    padding-left: 9%;
-    text-transform: uppercase;
-    font-weight: bold;
-    color:var(--accent-color);
-    padding-block:10;
-    text-align: center;
-    padding-block: 6%;
-    font-size:2em;
-    
-    
-}/*  */
-
-
-.card-reveal{
-    opacity: 0.85;
+.project img {
+  max-width: 100%;
+  height: auto;
+  display: block;
+  margin: 0 auto;
+  transition: transform 0.3s ease-in-out; 
 }
-#description{
+
+.project .pa-3 {
+  transition: transform 0.3s ease-in-out;
+}
+
+.project:hover .pa-3 {
+  transform: scale(1.05);
+}
+
+.project:hover img {
+  transform: scale(1.1);
+}
+
+.portfolio > h2{
+  padding-left: 9%;
+  text-transform: uppercase;
+  font-weight: bold;
+  color:var(--accent-color);
+  padding-block:10;
+  text-align: center;
+  padding-block: 6%;
+  font-size:2em;
+}
+
+.card-reveal {
+  opacity: 0.85;
+}
+
+#description {
   font-size: 18px;
 }
-#project_name{
- font-size:28px;
+
+#project_name {
+  font-size:28px;
 }
 
-.tech-icons{
-    position: absolute;
-    
-    justify-content: space-around;
-    padding:1px;
-    margin:2px;
-    bottom: 0px;
+.tech-icons {
+  position: absolute;
+  justify-content: space-around;
+  padding: 1px;
+  margin: 2px;
+  bottom: 0px;
+  width: 100%;
+  left: 0;
+  right: 0;
+  margin: auto;
+}
 
+.portfolio > h2 {
+  margin-right: 10rem;
+}
+
+@media (min-width: 768px) {
+  .project {
+    width: 50%;
+  }
+}
+
+@media (min-width: 992px) {
+  .project {
+    width: 50%;
+    min-width: 30rem;
+  }
+}
+
+@media (max-width: 767px) {
+  .project {
+    width: 100%;
+  }
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -10,5 +10,19 @@ const myProjects= [
         technologiesUsed:"React Native, Expo, NodeJS, ExpressJS, SocketIO",
         description:"A fully functionnal mobile app",
         picture:"https://res.cloudinary.com/dlnvacfsc/image/upload/v1678300240/Delivair_dxfymu.png"
+    },
+    {
+        title:"Delivair",
+        technologiesUsed:"React Native, Expo, NodeJS, ExpressJS, SocketIO",
+        description:"A fully functionnal mobile app",
+        picture:"https://res.cloudinary.com/dlnvacfsc/image/upload/v1678300240/Delivair_dxfymu.png"
+    },
+    {
+        title:"Delivair",
+        technologiesUsed:"React Native, Expo, NodeJS, ExpressJS, SocketIO",
+        description:"A fully functionnal mobile app",
+        picture:"https://res.cloudinary.com/dlnvacfsc/image/upload/v1678300240/Delivair_dxfymu.png"
     }
 ]
+
+export default myProjects
