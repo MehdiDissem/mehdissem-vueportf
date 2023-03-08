8061407c7724659ca4ffbf98d4f02ff050b16ceb
Mehdidissem <mehdissem@gmail.com>
2023-03-08 07:23:31 +0100

    created banner

--- src/components/homepage.vue
+++ src/components/homepage.vue
@@ -1,10 +1,26 @@
 <template>
     <v-container fluid class='homepage ma-0 pa-0' app >
+        <navigation id="nav"></navigation>
+            <banner/>
     </v-container>
 </template>
 
 <script>
+import navigation from "./navigation.vue"
+import banner from "./banner.vue"
 export default {
-    name:'homepage'
+    name:'homepage',
+    components:{
+        navigation,banner
+    }
 }
 </script>
+
+<style scoped >
+#nav{
+    position: fixed;
+    z-index: 4;
+}
+
+
+</style>
