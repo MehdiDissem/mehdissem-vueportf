b2b5415c84c5c5e9ab21a8b38e83e797289d9b4e
Mehdidissem <mehdissem@gmail.com>
2023-03-11 23:29:14 +0100

    added some projects data

--- src/store/index.js
+++ src/store/index.js
@@ -27,6 +27,20 @@ const myProjects= [
         picture:"https://res.cloudinary.com/dlnvacfsc/image/upload/v1678034681/githubfetcher/Screenshot_2023-03-05_174350_oyjxts.png",
         link:"https://github.com/MehdiDissem/githubFetcher"
     },
+    {
+        title:"NodeJS-chatGBT",
+        technologiesUsed:"NodeJS",
+        description:"A simple ChatGBT interaction with nodeJS, using an API key, you can test the response in Thunder Client.",
+        picture:"https://res.cloudinary.com/dlnvacfsc/image/upload/v1678572975/chatgbt-nodejs_u1dmza.png",
+        link:"https://github.com/MehdiDissem/chatGBT-nodeJS"
+    },
+    {
+        title:"SocialMedia App",
+        technologiesUsed:"NodeJS",
+        description:"A fully working social media website, where you make your profile, post, comment, like, add people... all stored in a local database.",
+        picture:"https://res.cloudinary.com/dlnvacfsc/image/upload/v1678573154/image_2_suf4po.png",
+        link:"https://github.com/MehdiDissem/Social-Media-App"
+    },
 ]
 
 export default myProjects
