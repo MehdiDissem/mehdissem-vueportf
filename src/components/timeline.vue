86dfa51341ec7a60a067231f599fef0593391954
Mehdidissem <mehdissem@gmail.com>
2023-03-12 21:10:56 +0100

    fixed timeline render on different screens size

--- src/components/timeline.vue
+++ src/components/timeline.vue
@@ -42,63 +42,46 @@
     color: var(--accent-color);
     padding-top: 15px;
     text-align: center;
-    margin-left:-7%
-}
-    .backgr{
+    margin-left: -7%;
+    margin-bottom: 20px;
+  }
+  
+  .backgr{
     background-color: var(--main-bg-color);
-    height: 100vh;
-    width: 100vw;
     padding: 5%;
-    
-    }
+  }
   
   .timeline {
-    margin: 50px 0px;
-    
+    margin: 30px 0;
   }
   
   .timeline-item {
     position: relative;
     padding: 20px 0;
-    
   }
   
   .timeline-item::before {
-  content: '';
-  position: absolute;
-  left: -12px;
-  top: 51.8%;
-  transform: translateY(-50%);
-  height: 12px;
-  width: 12px;
-  border-radius: 50%;
-  background-color: white;
-  animation: pulse 2s ease-in-out infinite;
-}
-
-@keyframes pulse {
-  0% {
-    box-shadow: 0 0 0 0 rgba(255, 255, 255, 0.5);
+    content: '';
+    position: absolute;
+    left: -12px;
+    top: 50%;
+    transform: translateY(-50%);
+    height: 12px;
+    width: 12px;
+    border-radius: 50%;
+    background-color: white;
   }
-  70% {
-    box-shadow: 0 0 0 10px rgba(255, 255, 255, 0);
-  }
-  100% {
-    box-shadow: 0 0 0 0 rgba(255, 255, 255, 0);
-  }
-}
-  
-  
   
   .timeline-content {
-    font-size: 15px;
+    font-size: 16px;
     margin-left: 32px;
-    color:white;
+    color: white;
   }
   
   .timeline-position {
     margin-top: 0;
     margin-bottom: 10px;
+    font-size: 18px;
   }
   
   .timeline-responsibilities {
@@ -106,64 +89,55 @@
     margin-bottom: 0;
     padding-left: 20px;
     list-style-type: disc;
-  }
-  
-  .timeline-company {
-    color: var(--accent-color);
-    font-weight: bold;
-    text-align: right;
-  }
-
-  @media screen and (max-width: 768px)  {
-
-    #title{
-    width: 100vw;
-    text-transform: uppercase;
-    font-weight: bold;
-    color: var(--accent-color);
-    padding-top: 15px;
-    text-align: center;
-    margin-bottom:20px;
-}
-    .backgr{
-    background-color: var(--main-bg-color);
-    height: auto;
-    width: auto;
-      }
-  .timeline-content{
-    font-size: 5px;
-    margin-bottom:20px;
-  }
-  .timeline {
-    direction: vertical;
-  }
-  .desktop-only {
-    display:contents;
-  }
-  
-  
-  .timeline-position {
-    font-size: 17px;
-    margin-bottom:20px;
-  }
-  
-  .timeline-company {
-    margin-top: 10px;
-    margin-bottom:20px;
     font-size: 14px;
-    font-weight: bold;
-    text-align: center;
   }
   
-  .timeline-responsibilities {
-    margin-top: 5px;
+  .timeline-company {
+    color: var(--accent-color);
+    font-weight: bold;
+    text-align: center;
+    margin-top: 10px;
     margin-bottom: 20px;
-    list-style-type:disc;
-    font-size: large;
+    font-size: 16px;
   }
-  .timeline-item::before {
-    animation: none;
+  
+  @media screen and (max-width: 768px)  {
+    #title{
+      font-size: 20px;
+      margin-left: 0;
+    }
+  
+    .backgr{
+      padding: 2%;
+    }
+  
+    .timeline-content{
+      font-size: 14px;
+      margin-bottom: 10px;
+    }
+  
+    .timeline {
+      margin: 20px 0;
+    }
+  
+    .timeline-position {
+      font-size: 16px;
+    }
+  
+    .timeline-company {
+      font-size: 14px;
+      margin-top: 5px;
+      margin-bottom: 10px;
+    }
+  
+    .timeline-responsibilities {
+      margin-top: 5px;
+      margin-bottom: 10px;
+      font-size: 14px;
+    }
+  
+    .timeline-item::before {
+      display: none;
+    }
   }
-}
-
-  </style>
+</style>
